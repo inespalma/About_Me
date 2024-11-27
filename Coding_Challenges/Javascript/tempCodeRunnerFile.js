042dcc4ca894f38b87d3e3e3ef2fec610c8502be
Inês Palma <77118154+inespalma@users.noreply.github.com>
2024-11-27 16:16:37 +0000

    Coding Challenges

---
+++ Coding_Challenges/Javascript/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+// function oneBooleanTrue(boolean1, boolean2) {
+//   return (boolean1 && !boolean2) || (!boolean1 && boolean2);
+// }
+
+// console.log(oneBooleanTrue(true, true)); //true
+// console.log(oneBooleanTrue(false, true)); //true
+// console.log(oneBooleanTrue(true, false)); //true
+// console.log(oneBooleanTrue(false, false)); //false
